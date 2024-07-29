c0e4686875d443821214b4dc14648b6481819a9d
AbhishekPatil313 <abhishekpatil3311@gmail.com>
2024-07-29 11:48:55 +0530

    added

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -6,8 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 @SpringBootApplication
-public class Application {
+public class Application  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
@@ -19,6 +23,10 @@ public class Application {
 		for (String beanName : beanNames) {
 			System.out.println(beanName);
 		}
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(Application.class);
 	}
 	
 }
